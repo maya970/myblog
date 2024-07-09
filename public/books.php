e488a433b27ef4223c9e1acbf9beee446cc5f5fc
maya970 <85286329+maya970@users.noreply.github.com>
2024-07-09 18:47:22 +0800

    Create books.php

---
+++ public/books.php
@@ -0,0 +1,45 @@
+<?php
+$books = [
+    [
+        'title' => '清朝历史书籍',
+        'author' => '作者1',
+        'website_affiliation' => '网站1',
+        'category' => '国内历史',
+        'subcategory' => '清朝历史',
+        'submitter' => '提交人1',
+        'rating' => 4.5,
+        'link' => 'http://example.com/book1'
+    ],
+    [
+        'title' => '明朝历史书籍',
+        'author' => '作者2',
+        'website_affiliation' => '网站2',
+        'category' => '国内历史',
+        'subcategory' => '明朝历史',
+        'submitter' => '提交人2',
+        'rating' => 3.8,
+        'link' => 'http://example.com/book2'
+    ],
+    [
+        'title' => '罗马历史书籍',
+        'author' => '作者3',
+        'website_affiliation' => '网站3',
+        'category' => '国外历史',
+        'subcategory' => '罗马历史',
+        'submitter' => '提交人3',
+        'rating' => 4.2,
+        'link' => 'http://example.com/book3'
+    ],
+    [
+        'title' => '德国历史书籍',
+        'author' => '作者4',
+        'website_affiliation' => '网站4',
+        'category' => '国外历史',
+        'subcategory' => '德国历史',
+        'submitter' => '提交人4',
+        'rating' => 4.0,
+        'link' => 'http://example.com/book4'
+    ],
+    // 添加更多书籍信息
+];
+?>
